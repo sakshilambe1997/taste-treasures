9184b1a4c1127ba2ff9eea3efb4dbeb4be10bb56
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-08-30 14:23:51 +0530

    Routing setup added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from './views/Home/Home';
+import About from './views/About/About';
+import Contact from './views/Contact/Contact';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Home</h1>)
+
+const router= createBrowserRouter([
+
+{
+  path:"/",
+  element:<Home/>
+},
+
+{
+  path:"/about",
+  element:<About/>
+},
+
+{
+  path:"/contact",
+  element:<Contact/>
+},
+{
+  path:"*",
+  element:<h1>Page Not Found</h1>
+}
+])
+
+root.render(
+  <RouterProvider router={router}/>
+);
+
 
